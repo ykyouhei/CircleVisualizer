e0e62307becf57b36c4a6faa3a82098154ab2baa
ykyouhei <kyouhei.lab@gmail.com>
2017-12-17 21:17:51 +0900

    Initial Commit

---
+++ CircleVisualizer/CircleVisualizer.h
@@ -0,0 +1,19 @@
+//
+//  CircleVisualizer.h
+//  CircleVisualizer
+//
+//  Created by 山口　恭兵 on 2017/12/17.
+//  Copyright © 2017年 kyo__hei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircleVisualizer.
+FOUNDATION_EXPORT double CircleVisualizerVersionNumber;
+
+//! Project version string for CircleVisualizer.
+FOUNDATION_EXPORT const unsigned char CircleVisualizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircleVisualizer/PublicHeader.h>
+
+
